1abe5eaa3295c5d93136d30e0c336a15122f3788
zhangkun123456 <980754689@qq.com>
2018-03-13 20:45:36 +0800

    case

--- app/Admin/Models/Cases.php
+++ app/Admin/Models/Cases.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cases extends Model
 {
-    //
+    //关联
+    public function casetype()
+    {
+        return $this->belongsTo(Casetypes::class);
+    }
+
+    //获取分类信息
+    public static function casetypelist(){
+    	return Casetypes::where('is_show', '=', 1)->pluck('title', 'id');
+    }
+   
+    //获取分类信息
+    public static function type($type_id)
+    {
+    	return Casetypes::where('id', '=', $type_id)->pluck('title');
+    }   
+
+
 }
