b3e00e5d8fcbcae4599cd1a209204c638018bdbb
zhangkun123456 <980754689@qq.com>
2018-03-13 20:43:39 +0800

    case

--- app/Admin/Controllers/CasesController.php
+++ app/Admin/Controllers/CasesController.php
@@ -24,8 +24,8 @@ class CasesController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('案例管理');
+            $content->description('列表');
 
             $content->body($this->grid());
         });
@@ -41,8 +41,8 @@ class CasesController extends Controller
     {
         return Admin::content(function (Content $content) use ($id) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('案例管理');
+            $content->description('修改');
 
             $content->body($this->form()->edit($id));
         });
@@ -57,8 +57,8 @@ class CasesController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('header');
-            $content->description('description');
+            $content->header('案例管理');
+            $content->description('创建');
 
             $content->body($this->form());
         });
@@ -73,10 +73,34 @@ class CasesController extends Controller
     {
         return Admin::grid(Cases::class, function (Grid $grid) {
 
-            $grid->id('ID')->sortable();
+            $grid->id('编号')->sortable();
+            $grid->title('标题');
+            $grid->address('项目地址'); 
+            $grid->area('地区');
 
-            $grid->created_at();
-            $grid->updated_at();
+            $grid->type_id('所属分类')->display(function ($type_id) {
+                $title =json_decode(Cases::type($type_id)) ;
+                $title = $title['0'];
+                return "<span style='font-size: 100%;' class='label label-warning'>$title</span>";
+            });
+
+            // $grid->img('图像')->image(); 
+
+            $grid->is_show('展示')->display(function ($is_show) {
+                return $is_show ? '是' : '否';
+            });
+
+            $grid->created_at('创建时间');
+            $grid->updated_at('修改时间');
+            
+            //查询过滤
+            $grid->filter(function ($filter){
+                // 去掉默认的id过滤器
+                $filter->disableIdFilter();
+                //设置需要的查询
+                $filter->like('title', '标题');
+            });
+
         });
     }
 
@@ -89,10 +113,32 @@ class CasesController extends Controller
     {
         return Admin::form(Cases::class, function (Form $form) {
 
-            $form->display('id', 'ID');
+            $form->display('id', '编号');
 
-            $form->display('created_at', 'Created At');
-            $form->display('updated_at', 'Updated At');
+            $form->text('title', '标题')->rules('required');
+
+            $form->image('img', '图片');
+            // $form->image('img', '图片')->move($dir, $name);
+
+            $form->text('address', '项目地址')->rules('required');
+
+            $form->text('area', '地区')->rules('required');
+
+            $form->editor('content', '项目介绍');
+
+            $form->radio('seat', '初始位置')->options(['200' => '200', '400' => '400', '600' => '600'])->default('200');
+
+            $form->select('type_id', '所属分类')->options( Cases::casetypelist() );
+
+            $states = [
+                'on'  => ['value' => 1, 'text' => '打开', 'color' => 'success'],
+                'off' => ['value' => 0, 'text' => '关闭', 'color' => 'danger'],
+            ];
+            $form->switch('is_show', '展示')->states($states);
+
+            $form->display( 'created_at', '创建时间');
+            $form->display( 'updated_at', '修改时间');
+
         });
     }
 }
